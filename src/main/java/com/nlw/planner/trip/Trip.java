938263b7454032418ceb6b80a1bbd4031ca2367f
Vitor Staub <vitorhugostaub@hotmail.com>
2024-07-08 23:02:48 -0300

    feat: add trip entity

---
+++ src/main/java/com/nlw/planner/trip/Trip.java
@@ -0,0 +1,39 @@
+package com.nlw.planner.trip;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String destination;
+
+    @Column(name = "starts_at", nullable = false)
+    private LocalDateTime starsAt;
+
+    @Column(name = "ends_at", nullable = false)
+    private LocalDateTime endsAt;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @Column(name = "owner_name", nullable = false)
+    private String ownerName;
+
+    @Column(name = "owner_email", nullable = false)
+    private String ownerEmail;
+}
