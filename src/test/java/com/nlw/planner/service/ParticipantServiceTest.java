ffb198007196e190f8c4fac92954e0790dd3106b
Vitor Staub <vitorhugostaub@hotmail.com>
2024-07-20 11:27:48 -0300

    fix: remove unused imports

--- src/test/java/com/nlw/planner/service/ParticipantServiceTest.java
+++ src/test/java/com/nlw/planner/service/ParticipantServiceTest.java
@@ -4,13 +4,10 @@ import com.nlw.planner.dto.TripRequestPayload;
 import com.nlw.planner.model.Participant;
 import com.nlw.planner.model.Trip;
 import com.nlw.planner.repository.PartipantRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
@@ -29,8 +26,6 @@ class ParticipantServiceTest {
     @InjectMocks
     private ParticipantService participantService;
 
-    private AutoCloseable closeable;
-
     @Test
     void registerParticipantsToEvent() {
         // Arrange
@@ -47,14 +42,13 @@ class ParticipantServiceTest {
         participantService.registerParticipantsToEvent(emails, trip);
 
         // Assert
-
         verify(repository, times(1)).saveAll(any(List.class));
     }
 
     @Test
     void registerParticipantToEvent() {
         // Arrange
-        String email = "vitor@gmail";
+        var email = "vitor@gmail";
         List<String> emails = Arrays.asList("vitor@gmmail", "test@outlook");
         var payload = new TripRequestPayload("São Paulo",
                 "2024-06-25T21:51:54.7342",
